e205024983b80e3c6ea960f5f14861636831369b
r3hmantom <r3hmantom@gmail.com>
2023-11-23 13:53:52 +0500

    Added: Headers for all functions

--- Beads.h
+++ Beads.h
@@ -1,15 +1,53 @@
 const int defaultColsH = 5;
 
-// BOARD
+// --------------------------------------------------------------------------------------
+// --------------------------------------------------------------------------------------
+
+
+// BOARD FUNCTIONS
 
 // Initializing Board
 void initBoard(int board[][defaultColsH], int ROWS, int COLS, int PLAYER1, int PLAYER2, int EMPTY);
 
-// Displaying Board
+// Displaying Board on console
 void displayBoard(int board[][defaultColsH], int ROWS, int COLS);
 
 // --------------------------------------------------------------------------------------
+// --------------------------------------------------------------------------------------
 
-void moveRight(int curPosition[][defaultColsH], int curR, int curC, int nextPosition[][defaultColsH], int nextR, int nextC);
+// MOVING BEADS IN AXIS
+
+// Right
+void moveRight(int curPosition[][defaultColsH], int curR, int curC, int EMPTY);
+
+// Left
+void moveLeft(int curPosition[][defaultColsH], int curR, int curC, int EMPTY);
+
+// Top
+void moveTop(int curPosition[][defaultColsH], int curR, int curC, int EMPTY);
+
+// Bottom
+void moveBottom(int curPosition[][defaultColsH], int curR, int curC, int EMPTY);
+
+// --------------------------------------------------------------------------------------
+// --------------------------------------------------------------------------------------
+
+// MOVING BEADS DIAGONALLY
+
+// Upper Right
+void upperRight(int curPosition[][defaultColsH], int curR, int curC, int EMPTY);
+
+// Upper Left
+void upperLeft(int curPosition[][defaultColsH], int curR, int curC, int EMPTY);
+
+// Lower Right
+void lowerRight(int curPosition[][defaultColsH], int curR, int curC, int EMPTY);
+
+// Lower Left
+void lowerLeft(int curPosition[][defaultColsH], int curR, int curC, int EMPTY);
+
+// --------------------------------------------------------------------------------------
+// --------------------------------------------------------------------------------------
+
 
 
