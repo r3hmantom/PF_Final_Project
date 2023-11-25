864d17634e17329e5806705decb166729dbbf814
r3hmantom <r3hmantom@gmail.com>
2023-11-25 18:23:15 +0500

    Refactoring code

--- BeadsGame.cpp
+++ BeadsGame.cpp
@@ -4,155 +4,166 @@
 
 using namespace std;
 
-// Constants and Global Variables
+// Constants
 const int ROWS = 5;
 const int COLS = 5;
 const int EMPTY = 0;
 const int PLAYER1 = 1;
 const int PLAYER2 = -1;
 const int BEAD_SIZE = 160;
+
+// Global Variables
 bool isPlayer1Turn = true;
 
 // Function Declarations
 bool loadTexture(sf::Texture& texture, const string& filename);
+void centerSpriteOrigins(sf::Sprite& sprite1, sf::Sprite& sprite2);
+// -----------------
 void processInput(sf::RenderWindow& window, int board[ROWS][COLS], int& selectedRow, int& selectedCol);
-void drawBeads(sf::RenderWindow& window, sf::Sprite& beadSprite1, sf::Sprite& beadSprite2, int board[][COLS] , int selectedRow, int selectedCol);
+void handleSelectionAndMovement(int board[ROWS][COLS], int gridX, int gridY, int& selectedRow, int& selectedCol);
+// -----------------
+void drawBeads(sf::RenderWindow& window, const sf::Sprite& beadSprite1, const sf::Sprite& beadSprite2, int board[ROWS][COLS], int selectedRow, int selectedCol);
+// -----------------
 
 int main() {
-    int board[ROWS][COLS];
-    initBoard(board, ROWS, COLS, PLAYER1, PLAYER2, EMPTY);
+	int board[ROWS][COLS] = {};
+	initBoard(board, ROWS, COLS, PLAYER1, PLAYER2, EMPTY);
 
-    sf::RenderWindow window(sf::VideoMode(800, 800), "12 Beads Game");
+	sf::RenderWindow window(sf::VideoMode(800, 800), "12 Beads Game");
 
-    sf::Texture beadTexture1, beadTexture2;
-    if (!loadTexture(beadTexture1, "bead1.png") || !loadTexture(beadTexture2, "bead2.png")) {
-        return -1;
-    }
+	sf::Texture beadTexture1, beadTexture2;
+	if (!loadTexture(beadTexture1, "bead1.png") || !loadTexture(beadTexture2, "bead2.png")) {
+		cerr << "Failed to load bead textures." << endl;
+		return EXIT_FAILURE;
+	}
 
-    sf::Sprite beadSprite1(beadTexture1), beadSprite2(beadTexture2);
+	sf::Sprite beadSprite1(beadTexture1), beadSprite2(beadTexture2);
+	centerSpriteOrigins(beadSprite1, beadSprite2);
 
-    // Set the origins of the sprites to their centers
-    sf::Vector2u textureSize1 = beadTexture1.getSize();
-    sf::Vector2u textureSize2 = beadTexture2.getSize();
-    beadSprite1.setOrigin(textureSize1.x / 2, textureSize1.y / 2);
-    beadSprite2.setOrigin(textureSize2.x / 2, textureSize2.y / 2);
+	int selectedRow = -1, selectedCol = -1;
 
-    int selectedRow = -1, selectedCol = -1;
+	while (window.isOpen()) {
+		processInput(window, board, selectedRow, selectedCol);
+		window.clear();
+		drawBeads(window, beadSprite1, beadSprite2, board, selectedRow, selectedCol);
+		window.display();
+	}
 
-    while (window.isOpen()) {
-        processInput(window, board, selectedRow, selectedCol);
-        window.clear();
-        drawBeads(window, beadSprite1, beadSprite2, board, selectedRow, selectedCol);
-        window.display();
-    }
-
-    return 0;
+	return EXIT_SUCCESS;
 }
 
 
-void handleSelectionAndMovement(int board[ROWS][COLS], int gridX, int gridY, int& selectedRow, int& selectedCol) {
-    static int originalRow = -1, originalCol = -1;
-    static bool moveMade = false; // Track if a valid move has been made
 
-    if (selectedRow == -1) {
-        // Check if the clicked bead belongs to the current player
-        if ((isPlayer1Turn && board[gridY][gridX] == PLAYER1) || (!isPlayer1Turn && board[gridY][gridX] == PLAYER2)) {
-            selectedRow = gridY;
-            selectedCol = gridX;
-            originalRow = gridY;
-            originalCol = gridX;
-            moveMade = false; // Reset moveMade as this is a new selection
-        }
-    }
-    else {
-        // Perform movement based on the position clicked
-        if ((gridY == selectedRow && gridX == selectedCol + 1) || (gridY == selectedRow && gridX == selectedCol + 2) ) {
-            moveRight(board, selectedRow, selectedCol, EMPTY, moveMade);
-        }
-        else if ((gridY == selectedRow && gridX == selectedCol - 1) || (gridY == selectedRow && gridX == selectedCol - 2)) {
-            moveLeft(board, selectedRow, selectedCol, EMPTY, moveMade);
-        }
-        else if ((gridX == selectedCol && gridY == selectedRow + 1) || (gridX == selectedCol && gridY == selectedRow + 2)) {
-            moveBottom(board, selectedRow, selectedCol, EMPTY, moveMade);
-        }
-        else if ((gridX == selectedCol && gridY == selectedRow - 1) || (gridX == selectedCol && gridY == selectedRow - 2)) {
-            moveTop(board, selectedRow, selectedCol, EMPTY, moveMade);
-        }
-        // Diagonal movement checks
-        else if ((gridX == selectedCol + 1 && gridY == selectedRow - 1) || (gridX == selectedCol + 2 && gridY == selectedRow - 2)) {
-            upperRight(board, selectedRow, selectedCol, EMPTY, moveMade);
-        }
-        else if ((gridX == selectedCol - 1 && gridY == selectedRow - 1) || (gridX == selectedCol - 2 && gridY == selectedRow - 2)) {
-            upperLeft(board, selectedRow, selectedCol, EMPTY, moveMade);
-        }
-        else if ((gridX == selectedCol + 1 && gridY == selectedRow + 1) || (gridX == selectedCol + 2 && gridY == selectedRow + 2)) {
-            lowerRight(board, selectedRow, selectedCol, EMPTY, moveMade);
-        }
-        else if ((gridX == selectedCol - 1 && gridY == selectedRow + 1) || (gridX == selectedCol - 2 && gridY == selectedRow + 2)) {
-            lowerLeft(board, selectedRow, selectedCol, EMPTY, moveMade);
-        }
-
-        // Reset selection
-        selectedRow = -1;
-        selectedCol = -1;
-
-        // Only switch turns if a valid move was made
-        if (moveMade) {
-            isPlayer1Turn = !isPlayer1Turn;
-            moveMade = false; // Reset for the next player's turn
-        }
-
-        // Reset original position for the next selection
-        originalRow = -1;
-        originalCol = -1;
-    }
-}
+// Handling sprite images
 bool loadTexture(sf::Texture& texture, const string& filename) {
-    if (!texture.loadFromFile(filename)) {
-        cerr << "Error loading " << filename << endl;
-        return false;
-    }
-    return true;
+	if (!texture.loadFromFile(filename)) {
+		cerr << "Error loading " << filename << endl;
+		return false;
+	}
+	return true;
+}
+void centerSpriteOrigins(sf::Sprite& sprite1, sf::Sprite& sprite2) {
+	sprite1.setOrigin(sprite1.getTexture()->getSize().x / 2, sprite1.getTexture()->getSize().y / 2);
+	sprite2.setOrigin(sprite2.getTexture()->getSize().x / 2, sprite2.getTexture()->getSize().y / 2);
+}
+
+
+// Handling selection an inputs
+bool isValidGridPosition(int x, int y) {
+	return x >= 0 && x < COLS && y >= 0 && y < ROWS;
 }
 void processInput(sf::RenderWindow& window, int board[ROWS][COLS], int& selectedRow, int& selectedCol) {
-    sf::Event event;
-    while (window.pollEvent(event)) {
-        if (event.type == sf::Event::Closed) {
-            window.close();
-        }
+	sf::Event event;
+	while (window.pollEvent(event)) {
+		if (event.type == sf::Event::Closed) {
+			window.close();
+		}
 
-        if (event.type == sf::Event::MouseButtonPressed && event.mouseButton.button == sf::Mouse::Left) {
-            int gridX = event.mouseButton.x / BEAD_SIZE;
-            int gridY = event.mouseButton.y / BEAD_SIZE;
+		if (event.type == sf::Event::MouseButtonPressed && event.mouseButton.button == sf::Mouse::Left) {
+			int gridX = event.mouseButton.x / BEAD_SIZE;
+			int gridY = event.mouseButton.y / BEAD_SIZE;
 
-            if (gridX >= 0 && gridX < COLS && gridY >= 0 && gridY < ROWS) {
-                handleSelectionAndMovement(board, gridX, gridY, selectedRow, selectedCol);
-            }
-        }
-    }
+			if (isValidGridPosition(gridX, gridY)) {
+				handleSelectionAndMovement(board, gridX, gridY, selectedRow, selectedCol);
+			}
+		}
+	}
 }
-void drawBeads(sf::RenderWindow& window, sf::Sprite& beadSprite1, sf::Sprite& beadSprite2, int board[][COLS], int selectedRow, int selectedCol) {
-    static float rotationAngle = 0.0f; // Static variable outside the loops
+void handleSelectionAndMovement(int board[ROWS][COLS], int gridX, int gridY, int& selectedRow, int& selectedCol) {
+	static int originalRow = -1, originalCol = -1;
+	static bool moveMade = false; // Track if a valid move has been made
 
-    for (int i = 0; i < ROWS; i++) {
-        for (int j = 0; j < COLS; j++) {
-            if (board[i][j] == EMPTY) {
-                continue; // Skip empty positions
-            }
+	if (selectedRow == -1) {
+		// Check if the clicked bead belongs to the current player
+		if ((isPlayer1Turn && board[gridY][gridX] == PLAYER1) || (!isPlayer1Turn && board[gridY][gridX] == PLAYER2)) {
+			selectedRow = gridY;
+			selectedCol = gridX;
+			originalRow = gridY;
+			originalCol = gridX;
+			moveMade = false; // Reset moveMade as this is a new selection
+		}
+	}
+	else {
+		// Perform movement based on the position clicked
+		if ((gridY == selectedRow && gridX == selectedCol + 1) || (gridY == selectedRow && gridX == selectedCol + 2)) {
+			moveRight(board, selectedRow, selectedCol, EMPTY, moveMade);
+		}
+		else if ((gridY == selectedRow && gridX == selectedCol - 1) || (gridY == selectedRow && gridX == selectedCol - 2)) {
+			moveLeft(board, selectedRow, selectedCol, EMPTY, moveMade);
+		}
+		else if ((gridX == selectedCol && gridY == selectedRow + 1) || (gridX == selectedCol && gridY == selectedRow + 2)) {
+			moveBottom(board, selectedRow, selectedCol, EMPTY, moveMade);
+		}
+		else if ((gridX == selectedCol && gridY == selectedRow - 1) || (gridX == selectedCol && gridY == selectedRow - 2)) {
+			moveTop(board, selectedRow, selectedCol, EMPTY, moveMade);
+		}
+		// Diagonal movement checks
+		else if ((gridX == selectedCol + 1 && gridY == selectedRow - 1) || (gridX == selectedCol + 2 && gridY == selectedRow - 2)) {
+			upperRight(board, selectedRow, selectedCol, EMPTY, moveMade);
+		}
+		else if ((gridX == selectedCol - 1 && gridY == selectedRow - 1) || (gridX == selectedCol - 2 && gridY == selectedRow - 2)) {
+			upperLeft(board, selectedRow, selectedCol, EMPTY, moveMade);
+		}
+		else if ((gridX == selectedCol + 1 && gridY == selectedRow + 1) || (gridX == selectedCol + 2 && gridY == selectedRow + 2)) {
+			lowerRight(board, selectedRow, selectedCol, EMPTY, moveMade);
+		}
+		else if ((gridX == selectedCol - 1 && gridY == selectedRow + 1) || (gridX == selectedCol - 2 && gridY == selectedRow + 2)) {
+			lowerLeft(board, selectedRow, selectedCol, EMPTY, moveMade);
+		}
 
-            sf::Sprite& currentSprite = (board[i][j] == PLAYER1) ? beadSprite1 : beadSprite2;
-            currentSprite.setPosition(j * BEAD_SIZE + BEAD_SIZE / 2, i * BEAD_SIZE + BEAD_SIZE / 2); // Center position
+		// Reset selection
+		selectedRow = -1;
+		selectedCol = -1;
 
-            // Reset rotation for all beads
-            currentSprite.setRotation(0);
+		// Only switch turns if a valid move was made
+		if (moveMade) {
+			isPlayer1Turn = !isPlayer1Turn;
+			moveMade = false; // Reset for the next player's turn
+		}
 
-            // Apply rotation if this is the selected bead
-            if (i == selectedRow && j == selectedCol) {
-                rotationAngle += 0.6f; // Increment the rotation angle
-                currentSprite.setRotation(rotationAngle);
-            }
+		// Reset original position for the next selection
+		originalRow = -1;
+		originalCol = -1;
+	}
+}
 
-            window.draw(currentSprite);
-        }
-    }
+
+// Drawing Beads
+void drawBead(sf::RenderWindow& window, const sf::Sprite& sprite, int row, int col, int selectedRow, int selectedCol, float& rotationAngle) {
+	sf::Sprite tempSprite = sprite; // Copy to modify position and rotation
+	tempSprite.setPosition(col * BEAD_SIZE + BEAD_SIZE / 2, row * BEAD_SIZE + BEAD_SIZE / 2);
+	tempSprite.setRotation((row == selectedRow && col == selectedCol) ? (rotationAngle += 0.6f) : 0.0f);
+	window.draw(tempSprite);
+}
+void drawBeads(sf::RenderWindow& window, const sf::Sprite& beadSprite1, const sf::Sprite& beadSprite2, int board[ROWS][COLS], int selectedRow, int selectedCol) {
+	static float rotationAngle = 0.0f;
+
+	for (int i = 0; i < ROWS; i++) {
+		for (int j = 0; j < COLS; j++) {
+			if (board[i][j] == EMPTY) continue;
+
+			const sf::Sprite& currentSprite = (board[i][j] == PLAYER1) ? beadSprite1 : beadSprite2;
+			drawBead(window, currentSprite, i, j, selectedRow, selectedCol, rotationAngle);
+		}
+	}
 }
